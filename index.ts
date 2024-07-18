e9d6e4cf22d411400e29c28aebcd3546ef4876ba
Areesha-Kamran <areeshakamran777@gmail.com>
2024-07-18 17:05:01 +0500

    word_count_pro is a straightforward command-line tool that quickly counts words, characters, and lines in your text files.

---
+++ index.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+
+import chalk from "chalk";
+
+import inquirer from "inquirer";
+
+import chalkAnimation from "chalk-animation";
+
+async function welcome () {
+    let title = chalkAnimation.rainbow("\t\t\t\tWelcome to WordCountPro!")
+    await new Promise((resolve) => {
+        setTimeout(resolve,3000);
+    });
+    title.stop();
+}
+
+await welcome()
+
+let usr_ans = await inquirer.prompt(
+    [
+        {
+            name:"words",
+            type:"input",
+            message:console.log(chalk.magenta("Please Enter Your Sentence"))
+        }
+    ]
+);
+
+let word_count = usr_ans.words.trim().split(" ").length;
+console.log(chalk.green.bold.italic(`Your Sentence has ${word_count} Words.`));
